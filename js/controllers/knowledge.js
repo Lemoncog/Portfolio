70ce5c6105f686fb504e545c92e9486c38b29445
Jason Leeming <jasonleeming@Jason-Leemings-MacBook-Pro.local>
2014-05-04 16:58:51 +0100

    Removed word cloud replaced with simple text

--- js/controllers/knowledge.js
+++ js/controllers/knowledge.js
@@ -1,39 +1,16 @@
-var Dimensions = function(width, height) {
-	this.width = width;
-	this.height = height;
-}
+console.log("Knowlege start");
 
-var KnowledgeCloud = function(element, data, dimensions) {
-
-	var fill = d3.scale.category20();
-
-	  d3.layout.cloud().size([dimensions.width, dimensions.height])
-	      .words(data.map(function(d) {
-	        return {text: d, size: 14 + Math.random() * 80};
-	      }))
-	      .padding(5)
-	      .rotate(function() { return ~~(Math.random() * 2) * 90; })
-	      .font("Impact")
-	      .fontSize(function(d) { return d.size; })
-	      .on("end", draw)
-	      .start();
+$.getJSON("data/knowledge.json", function(data){
 	
-	  function draw(words) {
-	    d3.select(element).append("svg")
-	        .attr("width", dimensions.width)
-	        .attr("height", dimensions.height)
-	      .append("g")
-	        .attr("transform", "translate(" + dimensions.width/2 + "," + dimensions.height/2 + ")")
-	      .selectAll("text")
-	        .data(words)
-	      .enter().append("text")
-	        .style("font-size", function(d) { return d.size + "px"; })
-	        .style("font-family", "Impact")
-	        .style("fill", function(d, i) { return fill(i); })
-	        .attr("text-anchor", "middle")
-	        .attr("transform", function(d) {
-	          return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-	        })
-	        .text(function(d) { return d.text; });
-	  };
-};
+	var knowledgeElement = $( "#knowledge-feed" );
+	var knowlegeTemplate = $("#knowledge-item-template").html();
+	
+	for(var index = 0; index < data.length; index++)
+	{
+		var skillItem = data[index];
+		
+		knowledgeElement.append(skillItem + " ");
+	};
+});
+
+console.log("Knowlege end");
