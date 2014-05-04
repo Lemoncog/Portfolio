32a247e34cc2772198cc3b923979070e37c02801
Jason Leeming <jasonleeming@Jason-Leemings-MacBook-Pro.local>
2014-05-04 11:36:07 +0100

    Adding section for skills

---
+++ js/loremIpsumFiller.js
@@ -0,0 +1,13 @@
+var LoremIpsumFil = function() {
+	$( ".loremFill" ).each(function( index ) {
+	     $( this ).append(loremIpsum);
+	});
+}; 
+
+var loremIpsum = "<p>Lorem ipsum dolor sit amet, cu sensibus scribentur sea, sit quis libris doming at, qui no tota mutat. Quo populo salutandi ex. Eum ne deleniti efficiendi. Pro vide omnes id. Munere delicatissimi vis cu, tollit suscipit eum at.</p>" +
+"<p>Tollit soleat invenire vel id, eos in postea vivendum. Has nonumes aliquando ad. In eum sonet dolores molestiae. Legere insolens eleifend cu nam, vim delectus petentium id. Mei id reque tractatos, id nec dicta dicit sadipscing.</p>" +
+"<p>Labore abhorreant id ius, eu mea indoctum democritum. Quem periculis patrioque in nam, placerat mediocrem consequat duo ad. Cum quas magna no, in recusabo euripidis sit, in pro civibus iracundia scriptorem. Eu mea postea sensibus assueverit, cu eum nostro nusquam. At mel viris facilisis imperdiet, per et impetus eripuit perpetua, ei constituto elaboraret mei.</p>" +
+"<p>In pri graece pericula. Dolorem invenire expetendis nec ei. Altera delenit et vim, hinc aliquando ne has, propriae voluptatibus vel ne. Ne ridens facilisi ius. Cu nam illud perfecto liberavisse, ad utamur copiosae pri.</p>" +
+"<p>Rebum iudico pro ex, sed ut congue labitur. Nam dictas integre cu, per cu alia nostro. Alia vulputate quo ut. No dolore nullam persequeris vis, nominavi liberavisse complectitur est te, sapientem intellegat duo at. Cu dicat labitur antiopam vim, has erat appareat id. Invenire interesset nam eu, qui velit dolorem mediocrem ex.</p>"
+
+new LoremIpsumFil();
